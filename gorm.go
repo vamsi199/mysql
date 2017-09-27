904b7dffc82dbf5ac0a816f9a4472bfc56e965c3
vamsi199 <vamsi.1991992@gmail.com>
2017-09-27 17:07:54 -0400

    Added many to many relation

--- gorm.go
+++ gorm.go
@@ -1,27 +1,73 @@
 package main
 
 import (
-	//"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
-        _ "github.com/go-sql-driver/mysql"
+	//"fmt"
+	"time"
 )
 
-func main()  {
-	db,err := gorm.Open("mysql", "root:pass@(107.21.2.246:3306)/vamsigo")
-	if err!= nil {
+func main() {
+	own := Owner{
+		Firstname:"vamsi",
+		Lastname:"gorapalli",
+	}
+	bo := Book{
+		Name:"hello",
+		Publishtime:time.Now(),
+		OwnerID:own.ID,
+	}
+	db, err := gorm.Open("mysql", "root:pass@(52.91.38.6:3306)/test")
+	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
-	db.CreateTable(&Owner{})
+	db.SingularTable(true)
+	db.DropTableIfExists(&Owner{},&Book{},&Auther{})
+	db.CreateTable(&Owner{},&Book{},&Auther{})
+	db.Create(&own)
+	db.Create(&bo)
+	own1 := Owner{
+		Firstname:"vamsi1",
+		Lastname:"gorapalli1",
+	}
+	bo1 := Book{
+		Name:"hello1",
+		Publishtime:time.Now(),
+		OwnerID:own.ID,
+	}
+	db.Debug().Save(&own1)
+	db.Debug().Save(&bo1)
 
-
-
+	//db.Create(&own) //create a indexes for the first time
+	//own.Firstname = "vamsichanged"
+	//db.Debug().Save(&own) //to update indexes
+	//db.Delete(&own)
 
 }
+
 type Owner struct {
+	gorm.Model
 	Firstname string
-	Lastname string
+	Lastname  string
+	Books []Book
+}
+type Book struct {
+	gorm.Model
+	Name string
+	Publishtime time.Time
+	OwnerID uint `sql:"index"`
+	Authers []Auther `gorm:"many2many:books_authers"`
+
+}
+type Auther struct {
+	gorm.Model
+	Name string
+
 }
 
+/*func (o *Owner) TableName() string { //to change the Table namefrom owner to changed
+	return "changed"
 
-
+}*/
